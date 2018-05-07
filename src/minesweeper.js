937df359652a94bb80ee9a347c373be863a415f4
Kevin Tate <2kmtate@gmail.com>
2018-05-07 16:35:47 -0400

    minesweeper part 2

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,12 +1,17 @@
-const blankline = ' | | ';
-const guessLine = '1| | ';
-const bombLine = ' |B| ';
+printBoard = function(board) {
+    console.log('Current Board: ');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
 
-console.log('This is what an empty board would look like:');
-console.log(blankline);
-console.log(blankline);
-console.log(blankline);
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankline);
+var board = [
+    [' ', ' ', ' '], 
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+];
+
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
