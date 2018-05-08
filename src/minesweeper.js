8ff437705b8eaa3bbf919752f63765f3152908af
Kevin Tate <2kmtate@gmail.com>
2018-05-08 16:05:08 -0400

    wrote code for minesweeper part 3 but needs fixed

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,46 @@
-printBoard = function(board) {
-    console.log('Current Board: ');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+generatePlayerBoard = function(numberOfRows, numberOfColumns) {
+    var board = [];
+    for (i=0; i < numberOfRows; i++) {
+        var row = [];
+        for (j=0; j < numberOfColumns; j++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
 };
 
-var board = [
-    [' ', ' ', ' '], 
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-];
+generateBombBoard = function(numberOfRows, numberOfColumns, numberOfBombs) {
+    var board = [];
+    for (i=0; i < numberOfRows; i++) {
+        var row = [];
+        for (j=0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    return board;
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+    var numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        // Currently this functionality can add bombs on top of bombs -> this will be fixed later
+        var randomRowIndex = Math.floor(Math.random() * numberOfRows); 
+        var randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+};
+
+printBoard = function(board) {
+    console.log(board.map(function(row) {
+        row.join(' | ');
+    }).join('\n'));
+};
+
+var playerBoard = generatePlayerBoard(3, 4);
+var bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player board: ');
+printBoard(playerBoard);
+console.log('Bomb board: ');
+printBoard(bombBoard);
